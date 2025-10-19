effa89df9fbbf0cf9720894fa84cdcdd04cb60df
Ваше Имя <barynin.yuri@gmail.com>
2025-10-19 15:56:38 +0200

    style: add auth summary

--- src/api/auth.py
+++ src/api/auth.py
@@ -52,7 +52,7 @@ async def register_user(
     return {"status": "OK"}
 
 
-@router.get("/me")
+@router.get("/me", summary="Get current user info👨‍💻")
 async def get_me(user_id: UserIdDep, db: DBDep):
     return await AuthService(db).get_user(user_id)
 
